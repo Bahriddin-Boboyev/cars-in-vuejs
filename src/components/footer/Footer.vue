f168d5209d6a63f8c418bb5cad1fa14f4ee84cd7
Bahriddin <bahriddinboboyev997@gmail.com>
2024-02-25 12:20:11 +0500

    change footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,14 +1,27 @@
 <template>
-    <footer class="footer"></footer>
+    <footer class="footer">
+        <div class="footer-block container">
+            <span class="footer-terms">© Cars Market {{ yearNow }}</span>
+        </div>
+    </footer>
 </template>
 
-<script setup></script>
+<script setup>
+const yearNow = new Date().getFullYear();
+</script>
 
 <style scoped>
 .footer {
     width: 100%;
-    height: 100px;
+    padding: 40px 0;
     background-color: hsla(0, 0%, 95.3%, 0.75);
     box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.05);
+}
+
+.footer-terms {
+    font-size: 17px;
+    display: block;
+    text-align: end;
+    font-weight: 500;
 }
 </style>
