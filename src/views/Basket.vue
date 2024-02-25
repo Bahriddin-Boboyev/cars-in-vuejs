bfc3657ca060f938cb1770ec49fa70d81312f9a4
Bahriddin <bahriddinboboyev997@gmail.com>
2024-02-26 00:17:32 +0500

    start Basket page

--- src/views/Basket.vue
+++ src/views/Basket.vue
@@ -1,13 +1,7 @@
 <template>
-    <div>
-        Basket
-    </div>
+    <div class="container basket">Basket</div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style  scoped>
-
-</style>
+<style scoped></style>
